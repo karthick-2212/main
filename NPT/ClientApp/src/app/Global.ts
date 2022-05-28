c4d0b26f4c13a1f5a418aadbe99f77e6ee36a695
karthick-2212 <105214404+karthick-2212@users.noreply.github.com>
2022-05-28 13:59:49 +0530

    Update Global.ts

--- NPT/ClientApp/src/app/Global.ts
+++ NPT/ClientApp/src/app/Global.ts
@@ -1,4 +1,4 @@
-//import { SpeechConfig, AudioConfig, SpeechSynthesizer, ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+import { SpeechConfig, AudioConfig, SpeechSynthesizer, ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
 export class GlobalFunctions {
     public static IsNullorEmpty(input: string) {
         return input === undefined || input === null || input.match(/^ *$/) !== null;
@@ -6,35 +6,35 @@ export class GlobalFunctions {
 
     public static StandardPronunciation(fullname: string, selectedcountry: string) {
         console.log(fullname);
-        // var key = "7ef35f6306fa4d8c9f4effc70c5db688";
-        // var region = "eastus";
+        var key = "7ef35f6306fa4d8c9f4effc70c5db688";
+        var region = "eastus";
 
-        // const speechConfig = SpeechConfig.fromSubscription(key, region);
-        // const audioConfig = AudioConfig.fromDefaultSpeakerOutput();
+        const speechConfig = SpeechConfig.fromSubscription(key, region);
+        const audioConfig = AudioConfig.fromDefaultSpeakerOutput();
 
-        // // The language of the voice that speaks.
-        // speechConfig.speechSynthesisVoiceName = selectedcountry;
+        // The language of the voice that speaks.
+        speechConfig.speechSynthesisVoiceName = selectedcountry;
 
-        // // Create the speech synthesizer.
-        // var synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+        // Create the speech synthesizer.
+        var synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
 
 
-        // // Start the synthesizer and wait for a result.
-        // synthesizer.speakTextAsync(fullname,
-        //     function (result: any) {
-        //         if (result.reason === ResultReason.SynthesizingAudioCompleted) {
-        //             console.log("synthesis finished.");
-        //         } else {
-        //             console.error("Speech synthesis canceled, " + result.errorDetails +
-        //                 "\nDid you set the speech resource key and region values?");
-        //         }
-        //         synthesizer.close();
-        //         //synthesizer = null;
-        //     },
-        //     function (err: any) {
-        //         console.trace("err - " + err);
-        //         synthesizer.close();
-        //         //synthesizer = null;
-        //     });
+        // Start the synthesizer and wait for a result.
+        synthesizer.speakTextAsync(fullname,
+            function (result: any) {
+                if (result.reason === ResultReason.SynthesizingAudioCompleted) {
+                    console.log("synthesis finished.");
+                } else {
+                    console.error("Speech synthesis canceled, " + result.errorDetails +
+                        "\nDid you set the speech resource key and region values?");
+                }
+                synthesizer.close();
+                //synthesizer = null;
+            },
+            function (err: any) {
+                console.trace("err - " + err);
+                synthesizer.close();
+                //synthesizer = null;
+            });
     }
 }
